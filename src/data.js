a8e707a20ba93c106ad0985555031e71f167b64c
Bahulvgopal <145193440+Bahulvgopal@users.noreply.github.com>
2023-10-17 20:45:42 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,57 @@
+export const projects = [
+  {
+    title: "Registration Form",
+    subtitle: "HTML and CSS",
+    description:
+      "Welcome to my React journey! In this section, I'm excited to introduce you to where it all began. 'My First React' is a milestone in my web development experience, representing the moment I delved into the world of React and its limitless possibilities.",
+    image: "./pro1.PNG",
+  },
+  {
+    title: "Profile task",
+    subtitle: "HTML and CSS",
+    description:
+      "In the spirit of innovation and the ever-evolving digital landscape, I took on the challenge to 'bring every business online' The result of this exciting endeavor is the 'Fishly Fish Farm' project. It's a testament to the power of technology it holds for businesses of all sizes.",
+    image: "./pro2.PNG",
+  },
+  {
+    title: "Portfolio Website",
+    subtitle: "HTML and Bootstarp",
+    description:
+      "This website serves as a showcase of my work and skills, featuring a clean and responsive design that ensures a seamless user experience across various devices. HTML and Bootstrap were the key tools used to craft this professional website.",
+    image: "./project-4.gif",
+  },
+  {
+    title: "Letter Form",
+    subtitle: "JS , HTML and CSS",
+    description:
+      "Mobile app developed using JavaScript and Firebase. JavaScript powers the app's frontend, providing a dynamic and interactive user experience. Firebase, a powerful backend platform, handles data storage, authentication, and real-time updates.",
+    image: "./pro4.PNG",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Fishly Fish Farm's new website has been a game-changer for our business. The professional and user-friendly design has made it easier for our customers to explore our products and place orders. It's a testament to the power of technology and the dedicated work of the development team. We're thrilled with the results and look forward to the continued growth this website will undoubtedly bring to our business.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Amit K. Jose",
+    company: "Fishly",
+  },
+  {
+    quote:
+      "The shopping list mobile app has completely changed the way I approach grocery shopping. It's like having a personal assistant right in my pocket! I no longer have to rely on pen and paper or try to remember everything I need. The convenience this app offers is a game-changer, and I can't imagine shopping without it now. Kudos to the developer for creating such a user-friendly and practical solution. It's become an essential part of my routine!",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Mathew",
+    company: "",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "HTML 5",
+  "Node",
+  "CSS",
+  "Bootstrap",
+  "Python",
+];
